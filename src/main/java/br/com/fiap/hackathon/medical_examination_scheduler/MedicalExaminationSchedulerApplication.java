a5b4faa11d1930fd432281af448bf0f63b75ceda
Pedro Ono <pedroono@icloud.com>
2025-03-05 21:33:47 -0300

    Commitando projeto

---
+++ src/main/java/br/com/fiap/hackathon/medical_examination_scheduler/MedicalExaminationSchedulerApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.hackathon.medical_examination_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalExaminationSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalExaminationSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/hackathon/medical_examination_scheduler/MedicalExaminationSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.hackathon.medical_examination_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalExaminationSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
